db10e48c9736add23272cba106c5d48a37dc9011
Brian Pulfer <Brian.Pulfer@unige.ch>
2022-03-22 22:47:56 +0100

    Changed default learning rate from 0.001 to 0.0001

--- scripts/run_main.sh
+++ scripts/run_main.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 -u ${HOME}/cdp_fastflow/src/main.py --data /home/users/p/pulfer/cdp_fastflow/1x1 --epochs 100 --bs 8 --lr 0.001 --tp 0.3 --fc 8 --nl 8 --seed 0
+python3 -u ${HOME}/cdp_fastflow/src/main.py --data /home/users/p/pulfer/cdp_fastflow/1x1 --epochs 100 --bs 8 --lr 0.0001 --tp 0.3 --fc 8 --nl 8 --seed 0
--- src/utils.py
+++ src/utils.py
@@ -16,7 +16,7 @@ def parse_args():
     parser.add_argument(f"--{DATA_DIR}", type=str, help="Data root directory path")
     parser.add_argument(f"--{EPOCHS}", type=int, help="Number of epochs", default=1)
     parser.add_argument(f"--{BS}", type=int, help="Batch size", default=8)
-    parser.add_argument(f"--{LR}", type=float, help="Learning rate", default=0.001)
+    parser.add_argument(f"--{LR}", type=float, help="Learning rate", default=0.0001)
     parser.add_argument(f"--{TP}", type=float, help="Training data percentage", default=0.3)
     parser.add_argument(f"--{FC}", type=int, help="Features channels", default=8)
     parser.add_argument(f"--{NL}", type=int, help="Number of affine coupling layers", default=8)
