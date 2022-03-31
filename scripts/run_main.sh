ba8e8db49e924d6c267c9e6e14406ff6f5e9e49d
Brian Pulfer <Brian.Pulfer@unige.ch>
2022-03-31 15:12:06 +0200

    Adjusted model path.

--- scripts/run_main.sh
+++ scripts/run_main.sh
@@ -3,7 +3,7 @@
 python3 -u \
   ${HOME}/cdp_fastflow/src/main.py \
   --data /home/users/p/pulfer/cdp_fastflow/1x1 \
-  --model ${HOME}/cdp_fastflow/flow_model.pt \
+  --model ${HOME}/cdp_fastflow/flow_model_sd.pt \
   --epochs 500 \
   --bs 16 \
   --lr 0.001 \
