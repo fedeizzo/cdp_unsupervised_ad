85d14815180c404662efd35886ba59047b0af8b0
Brian Pulfer <Brian.Pulfer@unige.ch>
2022-06-10 16:11:01 +0200

    Larger train percentage and more epochs by default.

--- scripts/run_main.sh
+++ scripts/run_main.sh
@@ -20,13 +20,14 @@ do
       --data "${HOME}"/${PROJECT_DIR}/datasets/1x1 \
       --mode $MODE \
       --originals $ORIGINAL \
-      --epochs 150 \
+      --epochs 300 \
       --bs 64 \
       --lr 0.01 \
-      --tp 0.4 \
+      --tp 0.6 \
       --vp 0.1 \
       --seed $SEED \
-      --result_dir "${HOME}"/${PROJECT_DIR}/results/${ORIGINAL}/seed_${SEED}/${MODE}
+      --result_dir "${HOME}"/${PROJECT_DIR}/results/${ORIGINAL}/seed_${SEED}/${MODE} \
+      --no_train
     done
   done
 done
