0334b41305f94c58fa59b3f51c642a3b992519cb
Brian Pulfer <Brian.Pulfer@unige.ch>
2022-10-26 09:28:04 +0200

    Plotting ROC curves w.r.t synthetic codes and originals.

--- src/visualizations/plots.py
+++ src/visualizations/plots.py
@@ -17,16 +17,30 @@ font = {
 }
 matplotlib.rc('font', **font)
 
-# Paths
-PROJECT_DIR = "/Users/bp/Desktop/Projects/cdp_unsupervised_ad"
-DATA_DIR = os.path.join(PROJECT_DIR, "datasets/mobile")
-ORIG_DIR = os.path.join(DATA_DIR, "orig_phone/HPI55_printdpi812.8_printrun1_session1_InvercoteG")
-FAKE_DIR = os.path.join(DATA_DIR, "fake_phone/HPI55_printdpi812.8_printrun1_session1_InvercoteG_EHPI55")
-
 # Common strings
 IPHONE = "iPhone12Pro"
 SAMSUNG = "SamsungGN20U"
 CONF = "ss100_focal12_apperture1"
+PRINTER_X = "HPI55_printdpi812.8_printrun1_session1_InvercoteG"
+PRINTER_F = PRINTER_X + "_EHPI55"
+
+# Paths
+PROJECT_DIR = "/Users/bp/Desktop/Projects/cdp_unsupervised_ad"
+DATA_DIR = os.path.join(PROJECT_DIR, "datasets/mobile")
+ORIG_DIR = os.path.join(DATA_DIR, "orig_phone", PRINTER_X)
+FAKE_DIR = os.path.join(DATA_DIR, "fake_phone", PRINTER_F)
+
+ORIGINALS_RUN = 1
+ORIGINALS_IPHONE = os.path.join(DATA_DIR, "orig_phone", PRINTER_X,
+                                f"iPhone12Pro_run{ORIGINALS_RUN}_ss100_focal12_apperture1/rcod_hist")
+ORIGINALS_SAMSUNG = os.path.join(DATA_DIR, "orig_phone", PRINTER_X,
+                                 f"SamsungGN20U_run{ORIGINALS_RUN}_ss100_focal12_apperture1/rcod_hist")
+
+SYNTHETICS_RUN = 1
+SYNTHETIC_IPHONE = os.path.join(DATA_DIR, "synthetic_phone", PRINTER_X, "seed_0",
+                                f"iPhone12Pro_run{SYNTHETICS_RUN}_ss100_focal12_apperture1")
+SYNTHETIC_SAMSUNG = os.path.join(DATA_DIR, "synthetic_phone", PRINTER_X, "seed_0",
+                                 f"SamsungGN20U_run{SYNTHETICS_RUN}_ss100_focal12_apperture1")
 
 
 def mse(t, y):
@@ -34,6 +48,10 @@ def mse(t, y):
 
 
 def pcorr(t, y):
+    return cv2.matchTemplate(t.numpy().flatten(), y.numpy().flatten(), cv2.TM_CCOEFF).ravel()[0]
+
+
+def normalized_pcorr(t, y):
     return cv2.matchTemplate(t.numpy().flatten(), y.numpy().flatten(), cv2.TM_CCOEFF_NORMED).ravel()[0]
 
 
@@ -41,7 +59,7 @@ def ssim(t, y):
     return structural_similarity(t.numpy().squeeze(), y.numpy().squeeze())
 
 
-def plot_hist_and_roc(fns, fn_names, dataset, o_names, f_names, roc_type="default"):
+def plot_hist_and_roc(fns, fn_names, dataset, o_names, f_names, roc_type="default", plot_hist=True):
     metrics = {}
     for fn, fn_name in zip(fns, fn_names):
         o_scores = [[] for _ in range(len(dataset.x_dirs))]
@@ -59,18 +77,19 @@ def plot_hist_and_roc(fns, fn_names, dataset, o_names, f_names, roc_type="defaul
 
         metrics[fn_name] = (o_scores, f_scores)
 
-    # Plotting histograms
-    for metric in metrics.keys():
-        o_scores, f_scores = metrics[metric]
-        for name, o_s in zip(o_names, o_scores):
-            plt.hist(o_s, label=name, density=True)
+    if plot_hist:
+        # Plotting histograms
+        for metric in metrics.keys():
+            o_scores, f_scores = metrics[metric]
+            for name, o_s in zip(o_names, o_scores):
+                plt.hist(o_s, label=name, density=True)
 
-        for name, f_s in zip(f_names, f_scores):
-            plt.hist(f_s, label=name, density=True)
-        plt.title(f"Histogram with {metric}")
-        plt.legend()
-        plt.xlabel(metric)
-        plt.show()
+            for name, f_s in zip(f_names, f_scores):
+                plt.hist(f_s, label=name, density=True)
+            plt.title(f"Histogram with {metric}")
+            plt.legend()
+            plt.xlabel(metric)
+            plt.show()
 
     # Plotting ROC curves
     for o_idx, o_name in enumerate(o_names):
@@ -97,14 +116,14 @@ def plot_hist_and_roc(fns, fn_names, dataset, o_names, f_names, roc_type="defaul
             elif roc_type == "Pe":
                 plt.xlim((0, max(t)))
                 n = len(f_names)
-                pf_w, pm_w = (n-1) / n, 1/n
+                pf_w, pm_w = (n - 1) / n, 1 / n
                 plt.plot(t, pf_w * fpr + pm_w * (1 - tpr), label=f"Pe ({metric})")
                 plt.xlabel("Threshold value")
             else:
                 # Default ROC curve
                 plt.xlim((0, 1))
                 plt.plot(fpr, tpr, label=f"{metric} (area = {area:.2f})")
-                plt.plot([0, 1], [0, 1], linestyle="--")
+                plt.plot([0, 1], [0, 1], "--", color="gray")
         plt.title(f"Originals {o_name} and fakes", fontdict={"size": 18, "weight": "bold"})
         plt.legend()
         plt.savefig(f"{o_name} ({fn_names}) - ROC {roc_type}")
@@ -113,7 +132,10 @@ def plot_hist_and_roc(fns, fn_names, dataset, o_names, f_names, roc_type="defaul
 
 def main():
     # Directories of data
-    t_dir = os.path.join(DATA_DIR, "orig_template")
+    t_dir = os.path.join(DATA_DIR, "orig_template")  # Comparing with templates
+    # t_dir = os.path.join(SYNTHETIC_IPHONE)  # Comparing with synthetic codes
+    # t_dir = os.path.join(ORIGINALS_IPHONE)  # Comparing with original codes
+
     x_dirs = [os.path.join(ORIG_DIR, f"{IPHONE}_run{run}_{CONF}/rcod_hist") for run in range(1, 7)]
     # x_dirs.extend([os.path.join(ORIG_DIR, f"{SAMSUNG}_run{run}_{CONF}/rcod_hist") for run in range(1, 7)])
     f_dirs = [os.path.join(FAKE_DIR, f"{IPHONE}_run{run}_{CONF}/rcod_hist") for run in range(1, 7)]
@@ -130,7 +152,8 @@ def main():
     print(f"Dataset loaded. Found {len(dataset)} shared CDP across all directories.")
 
     # Plots
-    plot_hist_and_roc([mse, pcorr, ssim], ["MSE", "PCorr", "SSIM"], dataset, o_names, f_names, roc_type="flipped")
+    methods, names = [mse, pcorr, normalized_pcorr, ssim], ["MSE", "PCorr", "PCorr normalized", "SSIM"]
+    plot_hist_and_roc(methods, names, dataset, o_names, f_names, roc_type="Pe", plot_hist=False)
 
 
 if __name__ == "__main__":
